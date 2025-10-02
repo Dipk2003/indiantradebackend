ad3867f5050440f498117e4b5625e5076de1c234
Dipanshu Pandey <dipkpandey56@gmail.com>
2025-10-03 00:46:27 +0530

    d3

--- src/main/java/com/itech/itech_backend/config/WebConfig.java
+++ src/main/java/com/itech/itech_backend/config/WebConfig.java
@@ -20,7 +20,12 @@ public class WebConfig implements WebMvcConfigurer {
                     "http://127.0.0.1:3000",
                     "http://127.0.0.1:3001",
                     "https://localhost:3000",
-                    "https://localhost:3001"
+                    "https://localhost:3001",
+                    "https://indiantrademart-frontend-env.eba-phxzpszy.ap-south-1.elasticbeanstalk.com",
+                    "https://indiantrademart.com",
+                    "https://dir.indiantrademart.com",
+                    "https://vendor.indiantrademart.com",
+                    "https://admin.indiantrademart.com"
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS", "HEAD")
                 .allowedHeaders("*")
